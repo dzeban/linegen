385aa25b7f4d73122ddfe0cdd02f8ef8bdcd3043
Alex Dzyoba <alex@dzyoba.com>
2017-11-17 16:55:58 +0300

    Add arguments

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
+	"flag"
 	"os"
 )
 
-const bufsize = 65536
+var Len int
+
+func init() {
+	flag.IntVar(&Len, "len", 10, "Length of string to generate")
+	flag.Parse()
+
+}
+
+func main() {
+	printBuffered(Len)
+}
 
 func printBuffered(n int) {
+	const bufsize = 65536
+
 	buf := make([]byte, bufsize)
 	i := 0
 	for {
 		copy(buf[i:i+n], RandString(n))
 		i += n
-		if i >= len(buf) {
+		if i >= len(buf)-n {
 			os.Stdout.Write(buf)
 			i = 0
 		}
 	}
-}
-
-func main() {
-	n := 4096
-	printBuffered(n)
 }
